068201ff8656246a0ea0682a252452861a2839d8
CoryCampbell <corycampbell20@yahoo.com>
2024-02-16 21:10:36 -0600

    finished isPalindrome

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,10 @@
 var isPalindrome = function (x) {
-	let reversed = [];
-	//take number and reverse it
-	for (let i = 0; i < x.length; i++) {
-		let current = x[i];
-		console.log("current: ", current);
-		reversed.push(current);
-	}
-	console.log(x);
-	console.log(reversed);
-	//compare reversed to original
-	if (x === reversed) return true;
-	else return false;
+	const original = x.toString();
+	let result = true;
+
+	let reversed = original.split("").reverse().join("");
+
+	if (reversed !== original) result = false;
+
+	return result;
 };
