770c65c638a7a3512fedb68594180a7985b40171
CoryCampbell <corycampbell20@yahoo.com>
2024-02-26 21:59:06 -0600

    start of DSA

---
+++ February/longestCommonPrefix.js
@@ -0,0 +1,11 @@
+var longestCommonPrefix = function(strs) {
+    console.log("strs: ", strs)
+
+    let prefix = "";
+
+    foreach (word in strs) {
+        console.log(word)
+    }
+
+    return prefix
+};
