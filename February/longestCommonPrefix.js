dc2f2aae31f77d462f993273dff37e146222d258
CoryCampbell <corycampbell20@yahoo.com>
2024-02-26 22:47:27 -0600

    solved

--- February/longestCommonPrefix.js
+++ February/longestCommonPrefix.js
@@ -1,8 +1,12 @@
-var longestCommonPrefix = function(strs) {
-    console.log("strs: ", strs)
+var longestCommonPrefix = function (strs) {
+	const copy = strs;
+	copy.sort();
 
-    let prefix = "";
+	for (let i = 0; i < copy[0].length; i++) {
+		if (copy[0][i] !== copy[copy.length - 1][i]) {
+			return copy[0].substr(0, i);
+		}
+	}
 
-
-    return prefix
+	return copy[0];
 };
