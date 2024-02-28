a14d990328c2902590298cd04cfe7803c51a7f52
CoryCampbell <corycampbell20@yahoo.com>
2024-02-27 23:30:03 -0600

    solved

--- February/validParentheses.js
+++ February/validParentheses.js
@@ -1,34 +1,20 @@
 var isValid = function (s) {
+	const pairs = {
+		"(": ")",
+		"{": "}",
+		"[": "]"
+	};
+
 	if (s[0] === ")" || s[0] === "}" || s[0] === "]") return false;
 	if (s.length % 2 !== 0) return false;
 	if (s[s.length - 1] === "(" || s[s.length - 1] === "{" || s[s.length - 1] === "[") return false;
-	/*
 
-    () - ! works
+	let stack = [];
 
-    {} - ! works
-
-    [] - ! works
-
-    (){}[] - ! works
-
-    {{[]}} - ! doesn't pass
-
-    */
-
-	//check for nested
-
-	//doenst solve nested parentheses
-	for (let i = 0; i < s.length - 1; i++) {
-		let current = s[i];
-		let last = s[i + 1];
-		console.log("current: ", current);
-		console.log("last: ", last);
-
-		if (current === "{" && last !== "}") return false;
-		if (current === "[" && last !== "]") return false;
-		if (current === "(" && last !== ")") return false;
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] === "(" || s[i] === "{" || s[i] === "[") stack.push(s[i]);
+		else if (pairs[stack.pop()] !== s[i]) return false;
 	}
 
-	return true;
+	return stack.length === 0;
 };
