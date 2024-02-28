606cb52e5b927119925e192893ece3b30f0b1ddc
CoryCampbell <corycampbell20@yahoo.com>
2024-02-27 22:46:47 -0600

    60/97 passing

--- February/validParentheses.js
+++ February/validParentheses.js
@@ -1,1 +1,30 @@
-var isValid = function (s) {};
+var isValid = function (s) {
+	/*
+
+    () - ! works
+
+    {} - ! works
+
+    [] - ! works
+
+    (){}[] - ! works
+
+    {{[]}} - ! doesn't pass
+
+    */
+
+	let valid = true;
+
+	for (let i = 0; i < s.length - 1; i++) {
+		let current = s[i];
+		let last = s[i + 1];
+		console.log("current: ", current);
+		console.log("last: ", last);
+
+		if (current === "{" && last !== "}") valid = false;
+		if (current === "[" && last !== "]") valid = false;
+		if (current === "(" && last !== ")") valid = false;
+	}
+
+	return valid;
+};
