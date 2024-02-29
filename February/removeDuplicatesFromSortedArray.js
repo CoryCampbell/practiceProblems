4b09201a7e67880df7af258d4f67160aeb280655
CoryCampbell <corycampbell20@yahoo.com>
2024-02-29 14:05:13 -0600

    solved

--- February/removeDuplicatesFromSortedArray.js
+++ February/removeDuplicatesFromSortedArray.js
@@ -1,1 +1,13 @@
-var removeDuplicates = function (nums) {};
+var removeDuplicates = function (nums) {
+	let used = new Set();
+	for (let i = 0; i < nums.length; i++) {
+		const current = nums[i];
+		if (used.has(current)) {
+			nums.splice(i, 1);
+			i--;
+		} else {
+			used.add(current);
+		}
+	}
+	return nums.length;
+};
