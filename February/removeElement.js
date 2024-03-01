8ecb20212ceb867599d52fcff43f3a957dfa1daf
CoryCampbell <corycampbell20@yahoo.com>
2024-02-29 22:05:57 -0600

    solved

--- February/removeElement.js
+++ February/removeElement.js
@@ -1,1 +1,11 @@
-var removeElement = function (nums, val) {};
+var removeElement = function (nums, val) {
+	for (let i = 0; i < nums.length; i++) {
+		let current = nums[i];
+
+		if (current === val) {
+			nums.splice(i, 1);
+			i--;
+		}
+	}
+	return nums.length;
+};
