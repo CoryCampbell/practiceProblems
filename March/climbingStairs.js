131caa2c33c092359cb75d3f6c52bbe1b952c69d
CoryCampbell <corycampbell20@yahoo.com>
2024-03-07 22:26:23 -0600

    solved climb stairs

--- March/climbingStairs.js
+++ March/climbingStairs.js
@@ -1,1 +1,12 @@
-var climbStairs = function (n) {};
+var climbStairs = function (n) {
+	let current = 0;
+	let prev = 1;
+	let two = 0;
+	for (let i = 0; i < n; i++) {
+		current = two + prev;
+		two = prev;
+		prev = current;
+	}
+
+	return prev;
+};
