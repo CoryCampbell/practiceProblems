e11f43627d80f567bf7a880e9a0265a13c6fca5d
CoryCampbell <corycampbell20@yahoo.com>
2024-03-12 00:16:20 -0500

    solved

--- March/deleteDuplicates.js
+++ March/deleteDuplicates.js
@@ -1,1 +1,14 @@
-var deleteDuplicates = function (head) {};
+var deleteDuplicates = function (head) {
+	let curr = head;
+
+	while (curr != null && curr.next != null) {
+		if (curr.val == curr.next.val) {
+			curr.next = curr.next.next;
+		} else {
+			curr = curr.next;
+		}
+	}
+	return head;
+};
+// Time: O(n) - must traverse through length of list to check for duplicates
+// Space O(1) - uses pointers in place to remove duplicates
