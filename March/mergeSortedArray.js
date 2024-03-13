7c8b38b13ef33138ee4aeb7f6206f40a50d6fd2a
CoryCampbell <corycampbell20@yahoo.com>
2024-03-13 00:18:54 -0500

    solved

--- March/mergeSortedArray.js
+++ March/mergeSortedArray.js
@@ -1,1 +1,8 @@
-var merge = function (nums1, m, nums2, n) {};
+var merge = function (nums1, m, nums2, n) {
+	//loop through all of nums2 list and append them to the end of nums1 list
+	for (let i = m, j = 0; j < n; i++, j++) {
+		nums1[i] = nums2[j];
+	}
+	//sort all of the nums
+	nums1.sort((a, b) => a - b);
+};
