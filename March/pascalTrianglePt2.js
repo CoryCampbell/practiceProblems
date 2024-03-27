252076073cd8f5aead52e6a9148f6916e1e2ca77
CoryCampbell <corycampbell20@yahoo.com>
2024-03-26 22:19:04 -0500

    pascall pt 2 solved

--- March/pascalTrianglePt2.js
+++ March/pascalTrianglePt2.js
@@ -1,1 +1,19 @@
-var getRow = function (rowIndex) {};
+var getRow = function (rowIndex) {
+	let triangle = [[1]];
+
+	if (!rowIndex) return triangle;
+
+	for (let i = 1; i < rowIndex + 1; i++) {
+		let last = triangle[i - 1];
+		let curr = [1];
+
+		for (let j = 1; j < last.length; j++) {
+			curr.push(last[j - 1] + last[j]);
+		}
+
+		curr.push(1);
+		triangle.push(curr);
+	}
+
+	return triangle[rowIndex];
+};
