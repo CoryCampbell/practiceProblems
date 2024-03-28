7660197fed39c1c829871b5bfbdfe63521faf812
CoryCampbell <corycampbell20@yahoo.com>
2024-03-28 00:14:47 -0500

    solved

--- March/validPalindrome.js
+++ March/validPalindrome.js
@@ -1,1 +1,10 @@
-var isPalindrome = function (s) {};
+var isPalindrome = function (s) {
+	//normalize data
+	//remove any non alphanumeric
+	//lowercase chars
+	s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+	//check
+	if (s.split("").reverse().join("") !== s) return false;
+	else return true;
+};
