704612af64758d566559eb9442130c816d968d95
CoryCampbell <corycampbell20@yahoo.com>
2024-04-02 22:25:24 -0500

    preorder solved

--- April/binaryTreePreorder.js
+++ April/binaryTreePreorder.js
@@ -1,1 +1,13 @@
-var preorderTraversal = function (root) {};
+function helper(root, res) {
+	if (!root) return;
+	res.push(root.val);
+	helper(root.left, res);
+	helper(root.right, res);
+}
+
+var preorderTraversal = function (root) {
+	let res = [];
+	if (!root) return res;
+	helper(root, res);
+	return res;
+};
