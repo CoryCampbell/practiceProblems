4543b4216b5cc4c9af5e5d24b8e7261e19a56c3c
CoryCampbell <corycampbell20@yahoo.com>
2024-04-04 23:17:21 -0500

    solved

--- April/majorityElement.js
+++ April/majorityElement.js
@@ -1,1 +1,22 @@
-var majorityElement = function (nums) {};
+var majorityElement = function (nums) {
+	let m = new Map();
+	m.set(nums[0], 1);
+
+	let highestCount = m.get(nums[0]);
+	let res = nums[0];
+
+	for (let i = 1; i < nums.length; i++) {
+		let curr = nums[i];
+
+		if (m.has(curr)) {
+			m.set(curr, m.get(curr) + 1);
+		} else m.set(curr, 1);
+
+		if (highestCount < m.get(curr)) {
+			highestCount = m.get(curr);
+			res = curr;
+		}
+	}
+
+	return res;
+};
