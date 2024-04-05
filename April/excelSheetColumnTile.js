0c2816b06ea9cf0379d211c1de74ecb68f7b6af4
CoryCampbell <corycampbell20@yahoo.com>
2024-04-04 22:34:18 -0500

    solved

--- April/excelSheetColumnTile.js
+++ April/excelSheetColumnTile.js
@@ -1,1 +1,13 @@
-var convertToTitle = function (columnNumber) {};
+var convertToTitle = function (columnNumber) {
+	let result = "";
+
+	while (columnNumber > 26) {
+		let last = columnNumber % 26;
+		last == 0 ? (last = 26) : 0;
+		result += String.fromCharCode(64 + last);
+		columnNumber = (columnNumber - last) / 26;
+	}
+
+	result += String.fromCharCode(64 + columnNumber);
+	return result.split("").reverse().join("");
+};
