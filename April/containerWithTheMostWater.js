730d6f8192aa54e2c3093c2d94fb2acf48bf3094
CoryCampbell <corycampbell20@yahoo.com>
2024-04-19 02:51:19 -0500

    solved

--- April/containerWithTheMostWater.js
+++ April/containerWithTheMostWater.js
@@ -1,1 +1,21 @@
-var maxArea = function (height) {};
+var maxArea = function (height) {
+	let left = 0;
+	let right = height.length - 1;
+	let highestArea = 0;
+
+	while (left < right) {
+		let width = right - left;
+		let h = Math.min(height[left], height[right]);
+		let water = width * h;
+
+		highestArea = Math.max(highestArea, water);
+
+		if (height[left] < height[right]) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+
+	return highestArea;
+};
