ce0b21902bd5300f907dfb4601444910184531c0
CoryCampbell <corycampbell20@yahoo.com>
2024-05-22 09:48:02 -0500

    solved

--- May/rotateImage.js
+++ May/rotateImage.js
@@ -6,10 +6,12 @@ var rotate = function (matrix) {
 
   //each iteration represents a ring around the matrix, start at the edges and going inwards
 	for (let i = 0; i < depth; i++) {
-    //the length each iteration will be 
+    //the length each iteration will be reduced by -2 each iteration
 		let len = n - 2 * i - 1;
+    //saving the opp value for faster computation
 		let opp = n - 1 - i;
 
+    //inner loop for the actual swapping of 4 elements per round
 		for (let j = 0; j < len; j++) {
 			let temp = matrix[i][i + j];
 			matrix[i][i + j] = matrix[opp - j][i];
