00bc0abbf5355e40782d0e6ff8f0c4508a714b60
CoryCampbell <corycampbell20@yahoo.com>
2024-05-23 10:55:22 -0500

    solved

--- May/groupAnagrams.js
+++ May/groupAnagrams.js
@@ -1,1 +1,22 @@
-var groupAnagrams = function (strs) {};
+var groupAnagrams = function (strs) {
+  //create a new map for storing anagram groups and how many words in each group
+	const mp = new Map();
+  //create final ans array
+	const ans = [];
+
+  //loop through each str in our argument strs array
+	for (const str of strs) {
+    //take the word and normalize it for comparisons
+		const sortedStr = str.split("").sort().join("");
+    //if our map has this sortedStr, we know that the str belongs in this group
+		if (mp.has(sortedStr)) {
+			ans[mp.get(sortedStr)].push(str);
+    //otherwise we create a new kvp pair insid our map to represent a new anagram group
+		} else {
+			mp.set(sortedStr, ans.length);
+			ans.push([str]);
+		}
+	}
+  
+	return ans;
+};
