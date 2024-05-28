e6222492b848910491e89360751b356bfbbabeba
CoryCampbell <corycampbell20@yahoo.com>
2024-05-28 14:09:57 -0500

    solved

--- May/reverseBits.js
+++ May/reverseBits.js
@@ -1,1 +1,12 @@
-var reverseBits = function (n) {};
+var reverseBits = function (n) {
+	//get the reversed value of our number
+	let reversedBit = n.toString(2).split("").reverse().join("");
+
+	//adds back any zeros that were lost in conversion
+	while (32 > reversedBit.length) {
+		reversedBit = reversedBit.concat("0");
+	}
+
+	//return the value in decimal form
+	return parseInt(reversedBit, 2);
+};
