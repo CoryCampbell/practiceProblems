2114063e59e03ff0aec691e7b692968f5ac205f6
CoryCampbell <corycampbell20@yahoo.com>
2024-05-29 12:41:53 -0500

    solved

--- May/numberOfOneBits.js
+++ May/numberOfOneBits.js
@@ -1,1 +1,14 @@
-var hammingWeight = function (n) {};
+var hammingWeight = function (n) {
+  //normalize the data
+	let chunk = n.toString(2);
+
+  //start a count to return
+  let count = 0;
+
+  //loop through every digit of our normalized data
+	for (let i = 0; i < chunk.length; i++) {
+		if (chunk[i] === "1") count++;
+	}
+
+	return count;
+};
