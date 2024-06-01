37da9a710f9b0cec9a74ce933b152bf728b101bc
CoryCampbell <corycampbell20@yahoo.com>
2024-06-01 14:15:27 -0500

    solved

--- June/containsDuplicate.js
+++ June/containsDuplicate.js
@@ -1,1 +1,17 @@
-var containsDuplicate = function (nums) {};
+var containsDuplicate = function (nums) {
+	//create a new set to record the first instance of each unique num
+	const used = new Set();
+
+	//loop through the whole nums array
+	for (let i = 0; i < nums.length; i++) {
+		let curr = nums[i];
+
+		//check if the curr number is already found in our used set, if found we return true because there are duplicates
+		if (used.has(curr)) return true;
+		//if the number isnt found, then its the first time its been used so we add it to our used set and continue
+		else used.add(curr);
+	}
+
+	//if we reach the end of the loop without finding a duplicate then we can return false
+	return false;
+};
