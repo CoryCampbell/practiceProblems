b5d46e4217eab8d81dacfa39e3909cd0500f4d3d
CoryCampbell <corycampbell20@yahoo.com>
2024-06-01 19:45:32 -0500

    practice

--- DemoCode/playWrightDemo/playwrightNotes.js
+++ DemoCode/playWrightDemo/playwrightNotes.js
@@ -4,12 +4,23 @@
 /*
 WHAT IS PLAYWRIGHT?
 
-A FRAMEWORK DESIGN TO PROVIDE RELIABLE END TO END TESTING
 
+A FRAMEWORK DESIGN TO PROVIDE RELIABLE END TO END TESTING, UI and/or API's tested together
+
+works with chromium/chrome/edge, firefox, WebKit
+
+provides automatic waiting, test generation, UI mode, etc.
+
+
+------------------------------------------------------------------------------------
 
 INSTALLING PLAYWRIGHT THROUGH VS CODE:
 
 USING THE PLAYWRIGHT EXTENSION PROVIDED BY MICROSOFT WE CAN RUN THE COMMAND "INSTALL PLAYWRIGHT".
+
+
+run by command:
+npx playwright test   ```for all tests```
 
 
 */
---
+++ DemoCode/playWrightDemo/tests/my-first-test.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require("@playwright/test");
+
+test("correct site page", async ({ page }) => {
+	await page.goto("https://news.ycombinator.com");
+
+	await expect(page).toHaveTitle(/Hacker News/);
+});
+
+test("has articles for collecting", async ({ page }) => {
+	await page.goto("https://news.ycombinator.com");
+
+	// await expect(page.locator('[class="athing"]')).toHaveValue();
+});
