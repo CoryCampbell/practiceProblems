0df39a7705fd539280414d7aff0fe750b5b4ba8b
CoryCampbell <corycampbell20@yahoo.com>
2024-06-11 20:05:17 -0500

    solved

--- June/reverseString.js
+++ June/reverseString.js
@@ -1,1 +1,21 @@
-var reverseString = function (s) {};
+var reverseString = function (s) {
+	//create some variables for solution
+	//this is all O(1) space complexity
+	let temp;
+	let length = s.length - 1;
+	let mid = Math.floor(s.length / 2);
+	let position = 0;
+
+	//while our position is less than the mid point we can swap the values of our array in place using a temp value
+	while (position < mid) {
+		let first = s[position];
+		let last = s[length];
+		temp = first;
+		s[position] = last;
+		s[length] = temp;
+		position++;
+		length--;
+	}
+
+	return s;
+};
