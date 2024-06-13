afb6fefb276545d75a2792dbd76d1ab0ae6ff892
CoryCampbell <corycampbell20@yahoo.com>
2024-06-13 15:51:15 -0500

    solved

--- June/firstUniqueCharacter.js
+++ June/firstUniqueCharacter.js
@@ -1,1 +1,12 @@
-var firstUniqChar = function (s) {};
+var firstUniqChar = function (s) {
+  //if our string doesn't have a length then we know theres neither duplicates or unique chars
+	if (!s || s.length === 0) return -1;
+
+  //loop through the length of our string
+	for (let i = 0; i < s.length; i++) {
+		const char = s[i];
+		if (s.indexOf(char) === s.lastIndexOf(char)) return i;
+	}
+
+	return -1;
+};
