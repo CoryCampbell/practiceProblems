58fbd8e1e12c218f8ce4916c7117cdcd30595ce7
CoryCampbell <corycampbell20@yahoo.com>
2024-06-14 19:59:19 -0500

    solved

--- June/fizzBuzz.js
+++ June/fizzBuzz.js
@@ -1,1 +1,23 @@
-var fizzBuzz = function (n) {};
+var fizzBuzz = function (n) {
+
+  //helper function for cleaner code
+	let helper = (num) => {
+		if (num % 15 === 0) {
+			return "FizzBuzz";
+		} else if (num % 3 === 0) {
+			return "Fizz";
+		} else if (num % 5 === 0) {
+			return "Buzz";
+		} else {
+			return num;
+		}
+	};
+
+	let res = [];
+
+	for (let i = 1; i <= n; i++) {
+		res.push(helper(`${i}`));
+	}
+
+	return res;
+};
