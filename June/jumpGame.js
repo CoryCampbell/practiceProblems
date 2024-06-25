d70655419f2cd7dcfd2281d6c6d2f2b197be041b
CoryCampbell <corycampbell20@yahoo.com>
2024-06-25 08:54:42 -0500

    solved

--- June/jumpGame.js
+++ June/jumpGame.js
@@ -1,3 +1,16 @@
 var canJump = function(nums) {
+  //store the last index value
+    let lastIndex = nums.length - 1;
 
+    //loop through the nums array starting one before last index; loop towards the first index
+    for (let i = nums.length - 2; i >= 0; i--) {
+      //if the sum of i and the current index value is greater than the last value
+      //we know that that number can traverse to the last index
+        if (i + nums[i] >= lastIndex) {
+          //
+            lastIndex = i;
+        }
+    }
+
+    return lastIndex === 0;
 };
