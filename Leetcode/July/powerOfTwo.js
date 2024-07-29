d3550c5c7936cb8c4710a07ff52865f0812a74c8
CoryCampbell <corycampbell20@yahoo.com>
2024-07-29 17:02:32 -0500

    solved

--- Leetcode/July/powerOfTwo.js
+++ Leetcode/July/powerOfTwo.js
@@ -1,1 +1,6 @@
-var isPowerOfTwo = function (n) {};
+var isPowerOfTwo = function (n) {
+  return n > 0 && (n & (n - 1)) === 0;
+};
+
+//using bitwise AND operation we can compare values which only returns zero when its power of two
+//1000 & 0111 = 0
