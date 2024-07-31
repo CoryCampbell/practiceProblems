7fd34d0e37ac342020244a40919ba100d865ca2b
CoryCampbell <corycampbell20@yahoo.com>
2024-07-31 11:39:57 -0500

    solved

--- Leetcode/July/binaryTreePaths.js
+++ Leetcode/July/binaryTreePaths.js
@@ -1,1 +1,19 @@
-var binaryTreePaths = function (root) {};
+var binaryTreePaths = function (root) {
+  let paths = [];
+
+  function findPaths(node, path) {
+    if (!node) return;
+
+    path.push(node.val);
+
+    if (!node.left && !node.right) {
+      paths.push(path.join("->"));
+    } else {
+      findPaths(node.left, [...path]);
+      findPaths(node.right, [...path]);
+    }
+  }
+
+  findPaths(root, []);
+  return paths;
+};
