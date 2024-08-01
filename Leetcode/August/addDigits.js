4d258e9c43cf6b9c2e62c382c02b5f72b78fdd20
CoryCampbell <corycampbell20@yahoo.com>
2024-08-01 13:07:56 -0500

    solved

--- Leetcode/August/addDigits.js
+++ Leetcode/August/addDigits.js
@@ -1,1 +1,8 @@
-var addDigits = function (num) {};
+var addDigits = function (num) {
+  //if our number is zero then return zero
+  if (num === 0) return 0;
+  //if our num modulo 9 is zero then we can return 9
+  else if (num % 9 === 0) return 9;
+  //otherwise we can return the result of num modulo 9, giving us the final addition of all indexes added together
+  else return num % 9;
+};
