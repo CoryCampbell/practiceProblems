936a1ec06990895bcbd1edba2635470b8bfeb6a9
CoryCampbell <corycampbell20@yahoo.com>
2024-08-07 20:29:14 -0500

    solved

--- Leetcode/August/wordPattern.js
+++ Leetcode/August/wordPattern.js
@@ -1,1 +1,27 @@
-var wordPattern = function (pattern, s) {};
+var wordPattern = function (pattern, s) {
+  const words = s.split(" ");
+  const charsToWords = new Map();
+  const wordsToChars = new Map();
+
+  let i = 0;
+
+  for (const word of words) {
+    if (i === pattern.length) {
+      return false;
+    }
+
+    const currentChar = pattern[i];
+
+    if (!charsToWords.has(currentChar) && !wordsToChars.has(word)) {
+      charsToWords.set(currentChar, word);
+      wordsToChars.set(word, currentChar);
+    } else {
+      if (charsToWords.get(currentChar) !== word || wordsToChars.get(word) !== currentChar) {
+        return false;
+      }
+    }
+    ++i;
+  }
+
+  return i === pattern.length;
+};
