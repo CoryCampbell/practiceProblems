8a091774e3c513350ca542038a38079ab41c6f69
CoryCampbell <corycampbell20@yahoo.com>
2024-08-21 00:47:12 -0500

    solved

--- Leetcode/August/reverseVowels.js
+++ Leetcode/August/reverseVowels.js
@@ -1,1 +1,27 @@
-var reverseVowels = function (s) {};
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+var reverseVowels = function (s) {
+  let first = 0;
+  let second = s.length - 1;
+  let chars = s.split("");
+
+  while (first < second) {
+    while (first < second && !vowels.includes(s[first])) {
+      first++;
+    }
+
+    while (first < second && !vowels.includes(s[second])) {
+      second--;
+    }
+
+    if (first < second) {
+      let temp = chars[first];
+      chars[first] = chars[second];
+      chars[second] = temp;
+      first++;
+      second--;
+    }
+  }
+
+  return chars.join("");
+};
