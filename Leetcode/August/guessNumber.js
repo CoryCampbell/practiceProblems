1945991347423d048b4d29733a9bcffbf8c16c2e
CoryCampbell <corycampbell20@yahoo.com>
2024-08-26 15:25:48 -0500

    solved

--- Leetcode/August/guessNumber.js
+++ Leetcode/August/guessNumber.js
@@ -1,1 +1,18 @@
-var guessNumber = function (n) {};
+var guessNumber = function (n, left = 1) {
+  let start = 1;
+  let end = n;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (guess(mid) == 0) {
+      return mid;
+    } else if (guess(mid) === 1) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return -1;
+};
