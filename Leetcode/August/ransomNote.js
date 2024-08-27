29d7df447a2415a8a438d1137f040241b81a7f45
CoryCampbell <corycampbell20@yahoo.com>
2024-08-27 16:55:53 -0500

    solved

--- Leetcode/August/ransomNote.js
+++ Leetcode/August/ransomNote.js
@@ -1,1 +1,25 @@
-var canConstruct = function (ransomNote, magazine) {};
+var canConstruct = function (ransomNote, magazine) {
+  if (ransomNote.length > magazine.length) return false;
+
+  const m = new Map();
+
+  for (let i = 0; i < magazine.length; i++) {
+    const char = magazine[i];
+
+    if (m.has(char)) {
+      m.set(char, m.get(char) + 1);
+    } else {
+      m.set(char, 1);
+    }
+  }
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    const char = ransomNote[i];
+
+    if (m.has(char) && m.get(char) > 0) {
+      m.set(char, m.get(char) - 1);
+    } else return false;
+  }
+
+  return true;
+};
