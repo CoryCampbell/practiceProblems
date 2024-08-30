29b176ae43887afa6d5e3e9761840785794f5df1
CoryCampbell <corycampbell20@yahoo.com>
2024-08-30 17:54:46 -0500

    solved

--- Leetcode/August/binaryWatch.js
+++ Leetcode/August/binaryWatch.js
@@ -1,1 +1,16 @@
-var readBinaryWatch = function (turnedOn) {};
+var readBinaryWatch = function (num) {
+  let answer = [];
+
+  for (let h = 0; h < 12; h++) {
+    for (let m = 0; m < 60; m++) {
+      let ones = Number(h * 64 + m)
+        .toString(2)
+        .split("")
+        .filter((d) => d === "1").length;
+
+      if (ones === num) answer.push(m < 10 ? `${h}:0${m}` : `${h}:${m}`);
+    }
+  }
+
+  return answer;
+};
