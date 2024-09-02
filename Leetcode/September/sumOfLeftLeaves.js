fc2310ba2548356fb7c10f9bc009409cba6ac367
CoryCampbell <corycampbell20@yahoo.com>
2024-09-02 18:49:11 -0500

    solved

--- Leetcode/September/sumOfLeftLeaves.js
+++ Leetcode/September/sumOfLeftLeaves.js
@@ -1,1 +1,10 @@
-var sumOfLeftLeaves = function (root) {};
+var sumOfLeftLeaves = function (root, total = 0) {
+  if (!root) return total;
+
+  if (root.left && !root.left.left && !root.left.right) total += root.left.val;
+
+  total = sumOfLeftLeaves(root.left, total);
+  total = sumOfLeftLeaves(root.right, total);
+
+  return total;
+};
