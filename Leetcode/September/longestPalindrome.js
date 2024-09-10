84c4a23165e22f1f562e3ea4f85d5e39ab1e6bda
CoryCampbell <corycampbell20@yahoo.com>
2024-09-10 17:16:50 -0500

    solved

--- Leetcode/September/longestPalindrome.js
+++ Leetcode/September/longestPalindrome.js
@@ -1,1 +1,32 @@
-var longestPalindrome = function (s) {};
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s.length;
+
+  const chars = new Map();
+  let middle = false;
+  let count = 0;
+
+  s.split("").forEach((char) => {
+    if (chars.has(char)) {
+      chars.set(char, chars.get(char) + 1);
+    } else {
+      chars.set(char, 1);
+    }
+  });
+
+  const iter1 = chars.values();
+
+  for (let i = 0; i < chars.size; i++) {
+    let curr = iter1.next().value;
+
+    if (curr % 2 !== 0 && middle === false) {
+      middle = true;
+      count += curr;
+    } else if (curr % 2 !== 0 && middle === true) {
+      count += curr - 1;
+    } else {
+      count += curr;
+    }
+  }
+
+  return count;
+};
